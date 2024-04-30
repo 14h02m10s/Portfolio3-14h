da460ec56bc72b3661a1c3468248af9b3a1e1a63
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2024-04-30 11:00:20 +0200

    modifs textes prez + supp locomotive

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,4 @@ import { RouterLink, RouterView } from "vue-router";
 </template>
 
 <style scoped>
-/* header {
-  line-height: 1.5;
-  max-height: 100vh;
-} */
-
-/* .logo {
-  display: block;
-  margin: 0 auto 2rem;
-} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,28 +1,8 @@
 import './assets/main.css'
-// import './locomotive-scroll.min.js'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Clipboard from "v-clipboard"
-// import LocomotiveScroll from "locomotive-scroll";
-
-// export default {
-//     methods: {
-//         setLocomotiveScroll() {
-//             new LocomotiveScroll({
-//                 el: this.$refs.container,
-//                 smooth: true,
-//                 multiplier: 5,
-//             });
-//         },
-//     },
-//     mounted() {
-//         this.setLocomotiveScroll();
-//     },
-// };
-
-// import ScrollAnimation from './directives/scrollanimation'
-
 import { MotionPlugin } from '@vueuse/motion'
 import AOS from "aos"
 import "aos/dist/aos.css"
@@ -34,11 +14,4 @@ app.use(Clipboard)
 app.use(MotionPlugin)
 AOS.init();
 app.mount('#app')
-/*
- Register the object here globally as directive,
- similar if you register a component.
- But now with Vue.directive, instead of component.
- With this you can use v-scrollanimation
- in your whole application on each element.
-*/
-// Vue.directive('scrollanimation', ScrollAnimation);
+
