b75b264f05c31e5834bf8fd23e26e0343e96a956
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2024-05-03 16:11:35 +0200

    structuration de la page artefacteurs

--- src/components/artefacteurs.vue
+++ src/components/artefacteurs.vue
@@ -1,130 +1,38 @@
-
 <script setup>
-import Footer from "./Footer.vue";
+// import Footer from "./Footer.vue";
 </script>
 
 <template>
-  <div id="contact">
+  <div id="topArtef">
     <div class="titre">
       <h2>Artefacteurs</h2>
     </div>
-    <div
-      class="container-except-titre"
-      data-aos="fade-up"
-      data-aos-duration="600"
-    >
-      <div class="login-card-container">
-        <form
-          action="https://public.herotofu.com/v1/8cc09760-7f05-11ee-8bd5-e178f08d9cb2"
-          method="post"
-          accept-charset="UTF-8"
-          class="login-card-form"
-        >
-          <div class="login-card-form-2">
-            <div class="form-item">
-              <span class="form-item-icon material-symbols-rounded"
-                >person</span
-              >
-              <input
-                type="text"
-                placeholder="Nom"
-                id="nom"
-                name="lastName"
-                required
-              />
-            </div>
-            <div class="form-item">
-              <span class="form-item-icon material-symbols-rounded"
-                >person</span
-              >
-              <input
-                type="text"
-                placeholder="Prénom"
-                id="prenom"
-                name="firstName"
-                required
-              />
-            </div>
-          </div>
-
-          <div class="form-item">
-            <span class="form-item-icon material-symbols-rounded">mail</span>
-            <input
-              type="email"
-              placeholder="Email"
-              id="email"
-              name="email"
-              required
-            />
-          </div>
-          <div
-            style="
-              text-indent: -99999px;
-              white-space: nowrap;
-              overflow: hidden;
-              position: absolute;
-            "
-            aria-hidden="true"
-          >
-            <input
-              type="text"
-              name="_gotcha"
-              tabindex="-1"
-              autocomplete="off"
-            />
-          </div>
-          <div class="form-item">
-            <textarea
-              id="message"
-              name="message"
-              placeholder="Hello Fred, nous serions vraiment ravis de te compter parmi nous ! "
-              required
-            ></textarea>
-          </div>
-          <div class="container-btn-envoyer">
-            <button type="submit" class="btn">Envoyer</button>
-          </div>
-        </form>
-        <div>
-          <p>
-            <br />
-            <br />
-            Si vous préférez utiliser votre boite mail, <br />copiez mon adresse
-            mail en cliquant dessus :
-            <br />
-            <span
-              v-clipboard="'fchambinaudpro@gmail.com'"
-              @click="ToggleCopied"
-              class="hover-list"
-              ><b>fchambinaudpro@gmail.com</b></span
-            >.
-            <br />
-          </p>
-          <p :class="{ copy: !isCopied, copied: isCopied }">C'est copié !</p>
-        </div>
+    <div class="container1" data-aos="fade-up" data-aos-duration="600">
+      <div class="colonne1"></div>
+      <div class="colonne2"></div>
+      <div class="btn-return">
+        <a href="#topArtef">
+          <button class="btn-carre">
+            <span class="material-symbols-rounded">
+              keyboard_double_arrow_up
+            </span>
+          </button>
+        </a>
       </div>
     </div>
-    <!-- <div class="btn-return">
-      <a href="#head">
-        <button class="btn-carre">
-          <span class="material-symbols-rounded">
-            keyboard_double_arrow_up
-          </span>
-        </button>
-      </a>
-    </div> -->
-    <div class="z-index">
+    <!-- <div class="z-index">
       <Footer />
-    </div>
+    </div> -->
   </div>
 </template>
 
 <style scoped>
-#contact {
+#topArtef {
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  background: linear-gradient(to bottom, #1c657c, #1c4856 50%, #1c4856 50%);
+  /* justify-content: center; */
+  /* background: linear-gradient(to bottom, #1c657c, #1c4856 50%, #1c4856 50%); */
+  background: #d6d6d6;
   width: 100%;
   position: relative;
   padding-top: 5.1rem;
@@ -132,7 +40,7 @@ import Footer from "./Footer.vue";
   height: 100vh;
   min-height: 1050px;
 }
-#contact::after {
+/* #topArtef::after {
   content: "";
   background-image: url(../assets/fonds/fluffy-clouds-are-colored-by-a-setting-sun.png);
   position: absolute;
@@ -143,7 +51,7 @@ import Footer from "./Footer.vue";
   background-repeat: no-repeat;
   background-position: 50% 0;
   background-size: cover;
-}
+} */
 .titre {
   width: 100%;
   z-index: 1;
@@ -159,92 +67,6 @@ h2 {
   font-size: 2.2rem;
   letter-spacing: 0.1rem;
 }
-.container-except-titre {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  z-index: 1;
-}
-.login-card-container {
-  /* min-height: 80vh; */
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  padding: 2rem 1.5rem 0rem 1.5rem;
-  z-index: 4;
-  width: 100%;
-}
-.login-card-form {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 1.5rem;
-  width: 100%;
-}
-.login-card-form-2 {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  gap: 1.5rem;
-}
-.login-card-form .form-item {
-  position: relative;
-}
-.login-card-form .form-item .form-item-icon {
-  position: absolute;
-  top: 0.82rem;
-  left: 1.4rem;
-  font-size: 1.3rem;
-  opacity: 0.4;
-}
-input[type="text"] {
-  border: none;
-  outline: none;
-  background: rgba(255, 255, 255, 0.7);
-  padding: 1rem 1.5rem;
-  padding-left: calc(1rem * 3.2);
-  border-radius: 100px;
-  width: 100%;
-  transition: background 0.5s;
-}
-input[type="email"] {
-  border: none;
-  outline: none;
-  background: rgba(255, 255, 255, 0.7);
-  padding: 1rem 1.5rem;
-  padding-left: calc(1rem * 3.2);
-  border-radius: 100px;
-  width: 100%;
-  transition: background 0.5s;
-}
-.form-item {
-  width: 100%;
-}
-input:focus {
-  background: white;
-}
-#message {
-  border-radius: 20px;
-  width: 100%;
-  height: 10rem;
-  border: none;
-  outline: none;
-  padding: 1rem 1.5rem;
-  background: rgba(255, 255, 255, 0.7);
-  transition: background 0.5s;
-}
-#message:focus {
-  background: white;
-}
-textarea {
-  font-family: "Open sans";
-  font-size: 0.9rem;
-}
-
 button,
 button::after {
   -webkit-transition: all 0.3s;
@@ -276,12 +98,7 @@ button::after {
   z-index: -1;
 }
 .btn-carre::before,
-.btn::before {
-  height: 100%;
-  left: 0;
-  top: 0;
-  width: 0;
-}
+.btn::before,
 .btn-carre::after,
 .btn::after {
   height: 100%;
@@ -289,6 +106,13 @@ button::after {
   top: 0;
   width: 0;
 }
+/* .btn-carre::after,
+.btn::after {
+  height: 100%;
+  left: 0;
+  top: 0;
+  width: 0;
+} */
 .btn-carre:hover::after,
 .btn:hover::after {
   width: 100%;
@@ -296,38 +120,11 @@ button::after {
 p {
   /* font-size: 0.9rem; */
   font-weight: 500;
-  /* text-align: justify; */
   margin-bottom: 0.7rem;
   color: #fff;
   text-align: center;
 }
-ul {
-  color: #fff;
-}
-.copy {
-  font-size: 0.9rem;
-  font-weight: 600;
-  color: rgba(255, 255, 255, 0);
-  animation: changerCouleur 2s ease-in-out;
-}
-@keyframes changerCouleur {
-  0% {
-    color: rgba(255, 255, 255, 1);
-  }
-  100% {
-    color: rgba(255, 255, 255, 0);
-  }
-}
-.copied {
-  font-size: 0.9rem;
-  font-weight: 600;
-  color: rgba(255, 255, 255, 0);
-}
-.container-btn-envoyer {
-  display: flex;
-  justify-content: center;
-  padding-top: 2rem;
-}
+
 .btn-return {
   display: flex;
   justify-content: right;
@@ -344,22 +141,32 @@ ul {
   align-items: flex-start;
   z-index: 4;
 }
-.hover-list {
-  font-weight: 600;
-  font-size: 1rem;
-  color: #ff6c0b;
-  color: #ffffff;
-  box-shadow: inset 0 0 0 0 #ffffff;
-  padding: 0 0.25rem;
-  margin: 0 -0.25rem;
-  transition: color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
-  text-decoration: underline;
+/* .container-except-titre {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+} */
+.container1 {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  z-index: 1;
+  height: fit-content;
+  background-color: #f8f8f8;
 }
-.hover-list:hover {
-  color: #fff;
-  box-shadow: inset 250px 0 0 0 #e05900;
-  cursor: pointer;
-  /* transition: color 0.3s ease-in-out, box-shadow 0.3s ease-in-out; */
+.colonne1 {
+  background-color: rgb(183, 183, 183);
+  height: 200px;
+  width: 100%;
+}
+.colonne2 {
+  background-color: rgb(85, 85, 85);
+  height: 3000px;
+  width: 100%;
 }
 /* #contact:target {
   padding-top: 6rem;
@@ -376,7 +183,7 @@ ul {
   width: 100%;
 }
 @media (min-width: 471px) {
-  #contact {
+  #topArtef {
     min-height: 1050px;
   }
   /* #contact:target {
@@ -385,38 +192,33 @@ ul {
   } */
 }
 @media (min-width: 768px) {
-  #contact {
+  #topArtef {
     padding-top: 7.5rem;
   }
   /* #contact:target {
     padding-top: 9.056rem;
     margin-top: 0rem;
   } */
-  .login-card-container {
-    max-width: 44rem;
-    padding: 2rem 0rem;
-  }
-  .login-card-form-2 {
-    display: flex;
-    flex-direction: row;
-  }
 }
 @media (min-width: 1135px) {
-  #contact {
+  #topArtef {
     height: 100vh;
+    /* height: 3000px; */
     padding-top: 6.7rem;
+  }
+  .container1 {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: start;
+    width: 100%;
+    z-index: 1;
   }
   /* #contact:target {
     padding-top: 7rem;
     margin-top: -5rem;
   } */
-  .container-except-titre {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-  }
+
   h2 {
     font-size: 2.7rem;
   }
