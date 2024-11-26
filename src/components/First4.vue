f900e7e1dd0b1a4541b7c5be99b05fcbf094b6a2
14h02m10s <144254313+14h02m10s@users.noreply.github.com>
2024-11-26 14:45:41 +0100

    modif cv + modif 1 img dans google ux

--- src/components/First4.vue
+++ src/components/First4.vue
@@ -13,7 +13,7 @@ onBeforeUnmount(() => {
   });
 });
 const cvURL =
-  "https://14h02m10s.github.io/Portfolio3-14h/cv-porfolio-nov2024";
+  "https://14h02m10s.github.io/Portfolio3-14h/cv-porfolio-nov2024.pdf";
 </script>
 
 <template>
